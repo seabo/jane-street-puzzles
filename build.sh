045dfb6b9b6e2151871acf4471d0688e42dac86b
seabo <37074155+seabo@users.noreply.github.com>
2022-07-26 16:58:27 +0100

    Set up Netlify doc builds

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Our projects need nightly. Netlify already has `rustup` and `cargo` preinstalled.
+if [ "$NETLIFY" == "true" ] 
+then
+    rustup toolchain install nightly
+fi
+
+# Build docs for Andy the Ant.
+cd andy-the-ant
+export RUSTDOCFLAGS="--html-in-header ./src/docs-header.html -Z unstable-options --extend-css src/extend.css" 
+cargo doc --no-deps
+
+cd ..
+mkdir -p target/doc
+mv andy-the-ant/target/doc target
