7ed44853c856d97e7d8c0ff88792f3865267a935
seabo <37074155+seabo@users.noreply.github.com>
2023-04-07 13:58:43 +0100

    fixes

--- build.sh
+++ build.sh
@@ -7,9 +7,8 @@ then
 fi
 
 # Build docs.
-export RUSTDOCFLAGS="--html-in-header ./src/docs-header.html -Z unstable-options --extend-css src/extend.css" 
-cargo doc --no-deps --release
+export RUSTDOCFLAGS="--html-in-header docs-header.html -Z unstable-options --extend-css extend.css" 
+cargo doc --no-deps --release --open
 
 # Copy the Netlify _redirects file into the publish directory.
 cp _redirects target/doc/_redirects
-
